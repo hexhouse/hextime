de3bd8c6cbe52327bb515e430c9ed28f63a7fb43
melaniehoff <hoffmelanie@gmail.com>
2026-06-12 22:15:06 -0400

    Login title: plain inline img like hexnye, no border wrapper

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,8 +31,8 @@
 <div class="min-h-screen bg-black flex items-center justify-center px-4">
 	<div class="w-full max-w-xs text-center">
 
-		<h1 class="mb-8" style="font-size: 4.5rem; font-family: 'Skanaus-Display', sans-serif; line-height: 1.2; text-align: center;">
-			<span style="font-family: 'Pinyon Script', cursive;">HEX</span>&nbsp;<span style="display: inline-block; border: 8px solid transparent; border-image: url('/assets/timer.png') 100%; vertical-align: middle; line-height: 0; padding: 0.1em;"><img src="/assets/timer.png" alt="" style="width: 0.8em; display: block;" /></span>&nbsp;<span>time</span>
+		<h1 class="mb-8" style="font-size: 4.5rem; font-family: 'Skanaus-Display', sans-serif; line-height: 1.3; text-align: center;">
+			<span style="font-family: 'Pinyon Script', cursive;">HEX</span>&nbsp;<img src="/assets/timer.png" alt="" style="width: 0.8em; vertical-align: middle;" />&nbsp;<span>time</span>
 		</h1>
 
 		<div style="border: 12px solid transparent; border-image: url('/assets/timer.png') 100%; padding: 2rem;">
