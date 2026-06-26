ec34a83916cc40910237a8cb744a42398aeb8fbc
melaniehoff <hoffmelanie@gmail.com>
2026-06-26 16:57:19 -0400

    Explicitly enable session persistence and auto token refresh

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,5 +2,9 @@ import { createClient } from '@supabase/supabase-js'
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 
 export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
-	auth: { experimental: { passkey: true } }
+	auth: {
+		experimental: { passkey: true },
+		persistSession: true,
+		autoRefreshToken: true,
+	}
 })
